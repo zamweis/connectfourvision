871deadb0bd4274993bb843d9792c3282755de46
Den Elfie <denelfie@gmail.com>
2022-01-18 18:44:35 +0100

    removed debug files

--- cmake-build-debug/CMakeFiles/bildverarbeitung_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bildverarbeitung_autogen"
-  "bildverarbeitung_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bildverarbeitung_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
